e8d60a8225ab3b5d8927017ef928b60f79d58ebc
Leigh Kirk <leighkirk@digital.homeoffice.gov.uk>
2019-07-02 09:43:57 +0100

    Add redux state storage

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,17 @@ import App from './App';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import './styles/app.scss';
+import getStore from './getStore';
+import { Provider } from 'react-redux';
+
+const store = getStore();
 
 const render = (_App) => {
-    console.log("in render");
     if (typeof window !== 'undefined') {
         ReactDOM.render(
-            <_App/>,
+            <Provider store={store}>
+                <_App/>
+            </Provider>,
             document.getElementById('AppContainer')
         )
     }
